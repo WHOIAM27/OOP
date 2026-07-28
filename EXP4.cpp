54804d6194a5c7c43d767aedeb1565ea39134d8d
rohittodakr92@gmail.com <rohittodkar92@gmail.com>
2026-07-28 14:50:22 +0530

    EXP4.cpp

---
+++ EXP4.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+class Matrix
+{
+  private:
+    int row,col;
+    int a[10][10];
+  public:
+  void readMatrix()
+  {
+    cout<<"Enter row and colums";
+  }
+};
+int main()
+{
+
+  return 0;
+}
