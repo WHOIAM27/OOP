564491930c42867d13ed529064fb0f8b64e5dc76
Rohit Sandeep Todkar <rohittodkar92@gmail.com>
2026-08-03 11:44:29 +0530

    Update EXP5 .cpp

--- EXP5 .cpp
+++ EXP5 .cpp
@@ -1,2 +1,22 @@
 #include<iostream>
 using namespace std;
+class Student {
+protected:
+    int rollNo;
+public:
+    Student() {
+        rollNo = 0;
+        cout << "Student Default Constructor Called" << endl;
+    }
+    Student(int r) {
+        rollNo = r;
+        cout << "Student Parameterized Constructor Called" << endl;
+    }
+    Student(const Student &s) {
+        rollNo = s.rollNo;
+        cout << "Student Copy Constructor Called" << endl;
+    }
+    ~Student() {
+        cout << "Student Destructor Called" << endl;
+    }
+};
